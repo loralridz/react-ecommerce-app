2bfd632a1a7de868e5a1423391c7c7af7357299d
loralridz <noorrida2000@gmail.com>
2021-04-10 01:04:43 -0700

    auth changes-unsubribe in unmount

--- src/App.js
+++ src/App.js
@@ -4,18 +4,37 @@ import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import { Header } from "./components/header/header.component";
 import { SignInAndSignUp } from "./pages/signin-signup/signin-signup.component";
+import { auth } from "./firebase/firebase.utils";
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() { 
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route  path="/shop" component={ShopPage} />
-        <Route  path="/signin" component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+    this.state = {
+      currentUser: null
+    };
+  }
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
